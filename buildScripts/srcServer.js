b81165d8444e77b53d7897ac0df05a5c51264c24
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-01-29 22:01:45 +0800

    Added babel for transpiling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+// var express = require('express');
+// var path = require('path');
+// var open = require('open');
 
-var port = 3000;
-var app = express();
+// babel-node allows the use of modern syntax
+import express from 'express';
+import path  from 'path';
+import open from 'open';
+
+const port = 3000;
+const app = express();
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,4 @@
-var chalk = require('chalk'); // can color the output to the console
+// var chalk = require('chalk'); // can color the output to the console
+import chalk from 'chalk';  // babel-node allows the use of modern syntax
 
 console.log(chalk.green('Starting app in dev mode...'));
