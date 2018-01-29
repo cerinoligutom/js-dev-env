a9c2a8f876c565b3a9c34557b8befc7beb1d82a1
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-01-29 22:57:27 +0800

    Added ESLint

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 // var chalk = require('chalk'); // can color the output to the console
 import chalk from 'chalk';  // babel-node allows the use of modern syntax
 
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
